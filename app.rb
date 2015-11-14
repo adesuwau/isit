52348618bb117d6f6016cbb869d0683deac03c44
Adesuwa Ugbogbo <missadeugbogbo@gmail.com>
2015-11-14 11:58:55 -0500

    logger info, zip code params

--- app.rb
+++ app.rb
@@ -1,6 +1,11 @@
+require 'json'
+require 'pry'
+
 class App < Sinatra:: Base
 
-
+configure do
+  enable :logging
+end
 
 
 
@@ -8,8 +13,15 @@ get ("/") do
   render(:erb, :index)
 end
 
-post ("/") do
-
+post ("/new") do
+  @user_info = []
+  @user_info.push(:user_choice => params[:hot])
+    @user_info.push(:user_choice => params[:cold])
+        @user_info.push(:user_choice => params[:snow])
+            @user_info.push(:user_choice => params[:rain])
+  @user_info.push(:user_zip => params[:user_zip])
+  logger.info @user_info
+binding.pry
 end
 
 get ("/result") do
